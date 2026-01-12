0e8a575dc78cfefa6f98e2bfe0656b4ccc1fb147
zannguyen <nguyen2406gl@gmail.com>
2026-01-13 03:12:36 +0700

    change

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -283,48 +283,54 @@ const getUser = async (req, res, next) => {
  * @param {Function} next - The next middleware function to call if consent is given by the user to enable context based auth.
  */
 const addUser = async (req, res, next) => {
-  let newUser;
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  /**
-   * @type {boolean} isConsentGiven
-   */
-  const isConsentGiven = JSON.parse(req.body.isConsentGiven);
-
-  const defaultAvatar =
-    "https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg";
-  const fileUrl = req.files?.[0]?.filename
-    ? `${req.protocol}://${req.get("host")}/assets/userAvatars/${
-        req.files[0].filename
-      }`
-    : defaultAvatar;
-
-  const emailDomain = req.body.email.split("@")[1];
-  const role = emailDomain === "mod.socialecho.com" ? "moderator" : "general";
-
-  newUser = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: hashedPassword,
-    role: role,
-    avatar: fileUrl,
-  });
-
   try {
+    let newUser;
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const isConsentGiven =
+      req.body.isConsentGiven !== undefined
+        ? JSON.parse(req.body.isConsentGiven)
+        : false;
+
+    const defaultAvatar =
+      "https://raw.githubusercontent.com/nz-m/public-files/main/dp.jpg";
+
+    const fileUrl = req.files?.[0]?.filename
+      ? `${req.protocol}://${req.get("host")}/assets/userAvatars/${
+          req.files[0].filename
+        }`
+      : defaultAvatar;
+
+    const emailDomain = req.body.email.split("@")[1];
+    const role = emailDomain === "mod.socialecho.com" ? "moderator" : "general";
+
+    newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+      role: role,
+      avatar: fileUrl,
+    });
+
     await newUser.save();
-    if (newUser.isNew) {
+
+    if (!newUser) {
       throw new Error("Failed to add user");
     }
 
     if (isConsentGiven === false) {
-      res.status(201).json({
+      return res.status(201).json({
         message: "User added successfully",
       });
     } else {
       next();
     }
   } catch (err) {
-    res.status(400).json({
-      message: "Failed to add user",
+    console.error("ADD USER ERROR:", err);
+    return res.status(500).json({
+      message: err.message,
+      stack: err.stack,
     });
   }
 };
